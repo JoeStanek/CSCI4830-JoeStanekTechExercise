42d4cde686ceaef5d4f914d3afbc5e20d905974a
JoeStanek <jstanek459@gmail.com>
2025-03-13 18:17:30 +0000

    began work on vies and templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import front_page
 from .views import add_task
 from .views import edit_task
 from .views import view_task
@@ -6,5 +7,9 @@ from .views import view_task
 
 
 urlpatterns = [
-    
+    path("", front_page, name="front_page"),
+    path("view/",view_task,name="view_task"),
+    path("add/",add_task, name="add_task"),
+    path("edit_task/<int:task_id>/<int:page_number/", edit_task, name="edit_task")
+    path("delete_task/<int:task_id>/<int:page_number/", delete_task, name="delete_task")
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
+
+def front_page():
+    pass
 
 def view_task():
     pass
 
-def add_task():
-    pass
+def add_task(request):
+    print("[DBG] add_task called.")
+    success = False
+    added_task = None
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            new_task = form.save()
+            success = True
+            added_task = new_task
+            return render(
+                request,
+                "add_task.html",
+                {"form":form, "added_task":added_task, "success":success},
+            )
+        else:
+            form = TaskForm()
+        return render(
+            request,
+            "add_task.html",
+            {"form":form, "added_task":added_task, "success":success}
+        )
+
 
 def edit_task():
+    pass
+
+def delete_task():
     pass
